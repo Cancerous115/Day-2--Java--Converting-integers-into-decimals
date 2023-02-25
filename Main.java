533224766723fa800137e894f7886d8ef15b14ea
Cancerous115 <107626947+Cancerous115@users.noreply.github.com>
2023-02-25 18:46:08 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[]args){
+    int number1=12;
+    double number2=5;
+   //division
+    System.out.println(number1/number2);
+  //modulo
+    System.out.println(number1%number2);
+    }
+}
